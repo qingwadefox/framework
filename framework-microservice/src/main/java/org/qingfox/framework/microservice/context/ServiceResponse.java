be047e2d78a52edf037a88007e5700be28287199
郑伟 <whoyoulove2003@163.com>
2018-10-29 17:07:39 +0800

    重写微服务CORE为接口代码

--- framework-microservice/src/main/java/org/qingfox/framework/microservice/context/ServiceResponse.java
+++ framework-microservice/src/main/java/org/qingfox/framework/microservice/context/ServiceResponse.java
@@ -17,11 +17,9 @@
  */
 package org.qingfox.framework.microservice.context;
 
-import org.qingfox.framework.microservice.MicroserviceClient;
-import org.qingfox.framework.microservice.ServiceMap;
-import org.qingfox.framework.microservice.ServiceResult;
+import java.io.Serializable;
 
-import com.alibaba.fastjson.JSONObject;
+import org.qingfox.framework.microservice.enums.RespCode;
 
 /**
  * .
@@ -29,45 +27,47 @@ import com.alibaba.fastjson.JSONObject;
  * @版权：福富软件 版权所有 (c) 2015
  * @author zhengwei3@ffcs.cn
  * @version Revision 1.0.0
- * @see:
- * @创建日期：2017年3月20日
- * @功能说明：
+ * @see: @创建日期：2017年3月20日 @功能说明：
  * 
  */
+public class ServiceResponse<T extends Serializable> implements Serializable {
 
-// 192.168.1.102
-// 255.255.255.0
-// 192.168.1.1
-// 355877064827431
+  private static final long serialVersionUID = -3655843737033626726L;
+  private RespCode code;
+  private String errorMsg;
+  private String errorStack;
+  private T result;
 
-public class ServiceResponse {
-	private String serviceId;
-	private int serviceStep = 0;
+  public String getErrorMsg() {
+    return errorMsg;
+  }
 
-	@SuppressWarnings("unchecked")
-	public <T> ServiceResult<T> sendURL(String url, ServiceMap<String, Object> param) {
-		ServiceMap<String, Object> headerParam = new ServiceMap<String, Object>();
-		headerParam.putSingle("SERVICE-ID", serviceId);
-		headerParam.putSingle("SERVICE-STEP", serviceStep);
-		String content = MicroserviceClient.post(url, new ServiceMap<String, String>(), headerParam);
-		return JSONObject.parseObject(content, ServiceResult.class);
-	}
+  public void setErrorMsg(String errorMsg) {
+    this.errorMsg = errorMsg;
+  }
 
-	@SuppressWarnings("unchecked")
-	public <T> ServiceResult<T> sendURL(String url) {
-		ServiceMap<String, Object> headerParam = new ServiceMap<String, Object>();
-		headerParam.putSingle("SERVICE-ID", serviceId);
-		headerParam.putSingle("SERVICE-STEP", serviceStep);
-		String content = MicroserviceClient.post(url, new ServiceMap<String, String>(), headerParam);
-		return JSONObject.parseObject(content, ServiceResult.class);
-	}
+  public T getResult() {
+    return result;
+  }
 
-	public void setServiceId(String serviceId) {
-		this.serviceId = serviceId;
-	}
+  public void setResult(T result) {
+    this.result = result;
+  }
 
-	public void setServiceStep(int serviceStep) {
-		this.serviceStep = serviceStep;
-	}
+  public RespCode getCode() {
+    return code;
+  }
+
+  public void setCode(RespCode code) {
+    this.code = code;
+  }
+
+  public String getErrorStack() {
+    return errorStack;
+  }
+
+  public void setErrorStack(String errorStack) {
+    this.errorStack = errorStack;
+  }
 
 }
